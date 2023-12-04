582de4355898f4934dbfb64c8dff9b2dc7c45176
Vishnu <vishnukun954AV@protonmail.com>
2023-12-04 11:51:59 +0545

    Added username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_060256) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_060555) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_060256) do
     t.datetime "confirmed_at"
     t.string "confirmation_token"
     t.datetime "confirmation_sent_at"
+    t.string "username"
   end
 
 end
