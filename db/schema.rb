3d66444fb5fc45ecdd6c3872395a08b6e42ce74d
Vishnu <vishnukun954AV@protonmail.com>
2023-12-04 11:42:45 +0545

    Added encrypted password to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_03_123206) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_055048) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_03_123206) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "unconfirmed_email"
+    t.string "encrypted_password"
   end
 
 end
