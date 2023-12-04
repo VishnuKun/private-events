fcc821948a3f9160a4607eed9193105df77e881a
Vishnu <vishnukun954AV@protonmail.com>
2023-12-04 11:46:04 +0545

    Added confirmation token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_055816) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_060030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_055816) do
     t.string "unconfirmed_email"
     t.string "encrypted_password"
     t.datetime "confirmed_at"
+    t.string "confirmation_token"
   end
 
 end
