9fa33477b5f89c328bae14f53f4c4268d0018a60
Vishnu <vishnukun954AV@protonmail.com>
2023-12-04 11:48:39 +0545

    Added confirmation sent attr

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_060030) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_060256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_060030) do
     t.string "encrypted_password"
     t.datetime "confirmed_at"
     t.string "confirmation_token"
+    t.datetime "confirmation_sent_at"
   end
 
 end
